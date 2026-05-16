47899774c148e4e892da3c0dfd5178ed2156afa2
Evgeny <Pushkarev>
2026-05-16 22:25:26 +0400

    Add CalculatorViewModel skeleton and operations enum

--- Calculator/CalculatorViewModel.swift
+++ Calculator/CalculatorViewModel.swift
@@ -1,8 +1,17 @@
-//
-//  CalculatorViewModel.swift
-//  Calculator
-//
-//  Created by Evgeny on 16.05.2026.
-//
-
 import Foundation
+
+@Observable
+class CalculatorViewModel {
+    // Текст, который будет выводиться на табло
+    var displayText: String = "0"
+    
+    // Перечисление доступных математических операций
+    enum CalcOperation {
+        case add, subtract, multiply, divide
+    }
+    
+    // Главная точка входа для нажатий кнопок из интерфейса
+    func receiveInput(_ button: String) {
+        // Логику обработки напишем на следующих этапах
+    }
+}
